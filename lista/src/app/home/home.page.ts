a2d586260f600c1a5c09d2250d8241e08988bdfe
kaioguaricafa <kaio.guari@edu.unifil.br>
2022-09-15 21:42:48 -0300

    upload

--- lista/src/app/home/home.page.ts
+++ lista/src/app/home/home.page.ts
@@ -1,51 +1,58 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {
+  constructor(private storage:Storage) {
+    this.storage.create();
   }
 
   variavel_lista = [];
   texto: string = "";
-  variavel_valor = [];
   valor: number = 0;
   soma = 0;
+  aux = 0;
 
+ngOnInit(){
+    this.atualizaLista();
+  }
 
-  adiciona() {
-    if (!(this.texto == "")) {
+atualizaLista() {
+    this.variavel_lista = [];
+    this.storage.forEach((value, key, index) => {
+  console.log(index);
+      this.variavel_lista.push([key, value]);
+    })
 
-      this.variavel_valor.push(this.valor);
-      this.variavel_lista.push(this.texto);
-      this.valor = 0;
-
-      this.texto = "";
-
-    }
     this.somar();
   }
 
-  remove(indice) {
-    this.variavel_lista.splice(indice, 1)
-    this.variavel_valor.splice(indice, 1)
-   this.somar()
+  async adiciona() {
+    if ( !(this.texto == "") && !(this.valor < 0)) {
+      this.variavel_lista.push(this.texto);
+      this.aux = this.variavel_lista.length - 1
+      await this.storage.set(this.aux.toString(), [this.texto, this.valor]);
+      this.atualizaLista();
+      this.valor = 0;
+      this.texto = "";
+    }
   }
 
- 
+  async remove(indice) {
+    await this.storage.remove(indice);
+    this.atualizaLista();
+  }
+
   somar() {
-    this.soma=0
-    this.variavel_valor.forEach(i => {
-    this.soma = this.soma + parseFloat (i);
+    this.soma = 0;
+    this.storage.forEach((value, key, index) => {
+      this.soma += value[1];
     })
   }
-
-
-  //*ngFor = "let elemento_da_lista of lista" no item
-  //[(ngModel)]="texto" no input
-
 }
